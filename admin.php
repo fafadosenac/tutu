e61279a6098a3ea0903a5c465f26345773ca8c8a
fafadosenac <119313888+fafadosenac@users.noreply.github.com>
2022-11-28 09:42:06 -0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+</head>
+<body>
+	<h1>Página Administrativa</h1>
+	<table>
+  <tr>
+    <th>Usuário</th>
+    <td>E-mail</td>
+    <td>Senha</td>
+  </tr>
+  <tr>
+  	<td>Usuário nome</td>
+    <td>E-mail da pessoa</td>
+    <td>Senha da pessoa</td>
+  </tr>
+</table>
+
+</body>
+</html>
